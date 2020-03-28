cb02eded405202dc750564135ae0a07067d84ed6
chenpeizhe <peizhe.chen@horizon.ai>
2020-03-28 22:22:00 +0800

    try to change to cpp style

--- wuserver.cpp
+++ wuserver.cpp
@@ -1,8 +1,12 @@
 #include <zmq.h>
 #include <string.h>
-#include <stdlib.h>
 #include <time.h>
 #include <assert.h>
+
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
 
 #define randof(num)  (int) ((float) (num) * random () / (RAND_MAX + 1.0))
 
@@ -23,9 +27,11 @@ int main (void)
         relhumidity = randof (50) + 10;
 
         //  Send message to all subscribers
-        char update [20];
-        sprintf (update, "%05d %d %d", zipcode, temperature, relhumidity);
-        zmq_send(publisher, update, strlen(update), 0);
+        std::ostringstream oss;
+        oss << std::setw(5) << std::setfill('0') << zipcode;
+        oss << ' ' << temperature << ' ' << relhumidity;
+        std::string out = oss.str();
+        zmq_send(publisher, out.data(), out.size(), 0);
     }
     zmq_close (publisher);
     zmq_ctx_destroy (context);
